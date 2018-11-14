91d4c7e023de2314a4565f75cd572ab861f289de
github username <github email address>
2018-11-14 16:01:54 -0500

    moremoremore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to '/'
+    else
+      render :edit
+    end
+  end
+
   def destroy
 
   end
